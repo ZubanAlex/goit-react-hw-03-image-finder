62c0eeb2af70d54d966d71f90400cb032d960118
ZubanAlex <zubanalex31@gmail.com>
2020-04-12 23:59:59 +0300

    Updates

---
+++ precache-manifest.014a538fdbbf08ae87d670362cbd3114.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "095d66b8bcf4537e69e2733316f11e53",
+    "url": "/goit-react-hw-03-image-finder/index.html"
+  },
+  {
+    "revision": "c09ea958b3b6a0f9a3f8",
+    "url": "/goit-react-hw-03-image-finder/static/css/main.07e6e391.chunk.css"
+  },
+  {
+    "revision": "c313dea8a07308c64ac0",
+    "url": "/goit-react-hw-03-image-finder/static/js/2.1b9f1163.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goit-react-hw-03-image-finder/static/js/2.1b9f1163.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c09ea958b3b6a0f9a3f8",
+    "url": "/goit-react-hw-03-image-finder/static/js/main.5a796370.chunk.js"
+  },
+  {
+    "revision": "ae3a2cafd41dba4ace62",
+    "url": "/goit-react-hw-03-image-finder/static/js/runtime-main.3da2d935.js"
+  }
+]);
